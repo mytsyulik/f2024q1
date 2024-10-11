2b7f5151532349ce8725a3bcd41af16a0eace60d
yulik <yulik>
2024-10-11 11:29:14 +0300

    add: BMW

---
+++ object.ts
@@ -0,0 +1,14 @@
+
+const BMW = {
+    
+    salon:'dark',
+    safety_systems:'ABS',
+    airbags:'Front',
+    comfort:'Engine autostart',
+    multimedia:'AUX or iPod',
+    
+}
+BMW.salon = 'dark'
+
+console.log(BMW)
+
---
+++ variable.ts
@@ -0,0 +1,4 @@
+const first_number=2875856
+const second_number=8
+const resuit=first_number/second_number  
+console.log(resuit)
