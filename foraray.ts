b5e7e6cdd150ab307369e7b7bb5937014618990f
yulik <yulik>
2024-11-01 11:42:00 +0300

    add:musicfunition.ts

---
+++ foraray.ts
@@ -0,0 +1,11 @@
+const  fruitArray=['apple','orange','banana','pear','plum']
+
+for(let y=0; y < fruitArray.length; y++){
+    if(fruitArray[y] === 'apple'){
+        console.log(`в данном массиве найден ${fruitArray[y]}`)
+    }
+}
+
+
+fruitArray[0]
+
