694a3ac0471e2c75b06b0af3c4c16f2663b3713b
yulik <yulik>
2024-11-22 11:32:49 +0300

    add:sort.js

---
+++ sort.js
@@ -0,0 +1,8 @@
+const words=['car','dog','apple','telepfone','hand','hair','long','friday']
+ const sortAlphabet= words.sort((a,b)=> a-b);
+ sortAlphabet.reverse()
+ 
+ console.log(sortAlphabet)
+
+
+
